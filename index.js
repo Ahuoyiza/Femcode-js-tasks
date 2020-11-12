2474b90fdb31f74abf06ece10c87c0e974460f26
Ahuoyiza <sophieoyiza@gmail.com>
2020-11-12 18:41:57 +0100

    working with variables

---
+++ index.js
@@ -0,0 +1,20 @@
+// task : working with variables
+
+let admin;
+let name;
+name =  "John" ;
+admin = name;
+
+alert(admin);
+
+// task: right variable naming
+ let planet = "earth";
+
+ let currentUser = "John";
+
+ // uppercase const?
+ const birthday = '18.04.1982';
+
+const age = someCode(birthday);
+// should birthday or age be uppercase?
+//answer: birthday , yes it is hardcoded age, no it has to be recalculated if birthday changes
